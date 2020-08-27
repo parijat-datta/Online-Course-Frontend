393cb65b6cdb6728aab923be8c45a6cbb9a47e6c
parijat-datta <parijatdutt@gmail.com>
2020-08-27 21:56:38 +0600

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+    import React from 'react';
+    import logo from './logo.png';
+    import './App.css';
+    import Course from './component/Course/Course';
+    import fakeData from './fakeData'
+    import { useState } from 'react';
+    import { useEffect } from 'react';
+    import Cart from './component/Cart/Cart'
+    import Nav from './component/Nav/Nav';
+    import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+    import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    function App() {
+
+    const [course, setCourse] = useState(fakeData);
+    const [count, setCount] = useState([]);
+    const [keyword, setKeyword] = useState("");
+
+
+
+
+    const handleAddCourse = (courses) => {
+    //Check if the course has been added in the cart or not!
+    if (count.includes(courses)) { alert("⚠️ This course has been already added, Enroll Into Another Course 😊") }
+    else {
+      const newCount = [...count, courses];
+      setCount(newCount);
+    }
+
+
+    }
+    //Converting any input string so that it is capitalized 
+    function capitalizeWords(str) {
+
+    let words = str.split(" ").map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+
+    })
+    return words.join(" ");
+    }
+
+    //filtering Courses based on the user search
+    useEffect(() => {
+    const newString = capitalizeWords(keyword);
+    const results = fakeData.filter(
+      (courseInfo) =>
+        courseInfo.courseName.includes(newString)
+    );
+    setCourse(results);
+    }, [keyword]);
+    return (
+
+
+    <div className="container-fluid main-web ">
+
+      <div className="head">
+        <img className="img-responsive headImage" src={logo} alt=""></img>
+        <h5>Build Your Career Online <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></h5>
+
+
+      </div>
+
+
+      <div className="row ">
+
+
+        <div className="col-md-9 main    "
+          style={{ padding: "10px", borderRight: "2px solid grey" }}>
+
+
+          <Nav setKeyword={setKeyword}></Nav>
+          <div className="courseDesign">
+
+
+            {
+
+              course.map((cd) =>
+
+                <Course className="" handleAddCourse={handleAddCourse} courseDescription={cd} key={cd.id}></Course>
+
+              )
+            }
+          </div>
+        </div>
+        <div className="col-md-3 cart">
+
+          <Cart cart={count}></Cart>
+        </div>
+      </div>
+
     </div>
-  );
-}
 
-export default App;
+    );
+    };
+
+    export default App;
---
+++ src/fakeData/data.js
@@ -0,0 +1,176 @@
+var data =[
+
+    {
+      "courseName":"Mobile App Development",
+      "instructor":"Mr. Parijat",
+      "duration":"4 Months",
+      "picture":"https://i.ibb.co/D9QTLdK/courses-online-1.jpg",
+      "id":1,
+      "price":57
+
+    },
+
+
+{
+      "courseName":"Python Tutorial",
+      "instructor":"Mr. Jhankar",
+      "duration":"6 Months",
+      "picture":"https://i.ibb.co/jhK780n/courses-online-1.png",
+      "id":2,
+      "price":87
+
+    },
+    
+
+    {
+        "courseName":"React Native",
+        "instructor":"Mr. Albert",
+        "duration":"2 Months",
+        "picture":"https://i.ibb.co/m4SdkdD/courses-online-2.jpg",
+        "id":3,
+        "price":47
+  
+      },
+
+      {
+        "courseName":"Graphics Design",
+        "instructor":"Miss Ankhi",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/5FDPkjQ/courses-online-2.png",
+        "id":4,
+        "price":110
+  
+      },
+
+  
+      {
+        "courseName":"Full Stack Web Development",
+        "instructor":"Mr Manik ",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/mRP40WB/courses-online-3.jpg",
+        "id":5,
+        "price":118
+  
+      },
+
+      
+      {
+        "courseName":"Create Your Own Course",
+        "instructor":"Mr Minhaz",
+        "duration":"5 Months",
+        "picture":"https://i.ibb.co/zhcs8Pd/courses-online-3.png",
+        "id":6,
+        "price":140
+  
+      },
+      {
+        "courseName":"Node JS & API",
+        "instructor":"Mr Nadim",
+        "duration":"3 Months",
+        "picture":"https://i.ibb.co/307yFC1/courses-online-4.jpg",
+        "id":7,
+        "price":150
+  
+      },
+
+      {
+        "courseName":"Python & Django",
+        "instructor":"Mr Shimanto",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/M6dRG91/courses-online-4.png",
+        "id":8,
+        "price":160
+  
+      },
+
+      {
+        "courseName":"PHP & MySQL",
+        "instructor":"Mr Saymon",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/QHgB4KN/courses-online-5.jpg",
+        "id":9,
+        "price":170
+  
+      },
+      
+
+
+      {
+        "courseName":"Mobile-APP Development Trends",
+        "instructor":"Mr Tonmoy",
+        "duration":"8 Months",
+        "picture":"https://i.ibb.co/wK2KBGB/courses-online-5.png",
+        "id":10,
+        "price":180
+  
+      },
+      {
+        "courseName":"Python Basic Concepts",
+        "instructor":"Mr Priyo",
+        "duration":"2 Months",
+        "picture":"https://i.ibb.co/Fg2QKkB/courses-online-6.jpg",
+        "id":11,
+        "price":210
+  
+      },
+
+      {
+        "courseName":"Python Full Course",
+        "instructor":"Mr Azahar",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/QCvcMpK/courses-online-7.jpg",
+        "id":12,
+        "price":170
+  
+      },
+
+      {
+        "courseName":"3D Game Development",
+        "instructor":"Mr Rehman",
+        "duration":"4 Months",
+        "picture":"https://i.ibb.co/zb2RNy0/courses-online-8.jpg",
+        "id":13,
+        "price":270
+  
+      },
+      
+
+      {
+        "courseName":"Rapid Mobile-APP DevOps",
+        "instructor":"Mr kazol",
+        "duration":"3 Months",
+        "picture":"https://i.ibb.co/8zbNC17/courses-online-9.jpg",
+        "id":14,
+        "price":250
+  
+      },
+
+      {
+        "courseName":"Web Development Trends",
+        "instructor":"Mr Badhon",
+        "duration":"3 Months",
+        "picture":"https://i.ibb.co/CK5xM2j/courses-online-10.jpg",
+        "id":15,
+        "price":"270"
+  
+      }
+    ]
+      
+      
+      
+      
+      export default data;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
